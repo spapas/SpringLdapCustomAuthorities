7b3286722e9421a17e88e11ee49bdd8888cdd34b
spapas <spapas@gmail.com>
2013-10-13 23:44:57 +0300

    New version

--- src/main/java/gr/serafeim/springldapcustom/CustomLdapAuthoritiesPopulator.java
+++ src/main/java/gr/serafeim/springldapcustom/CustomLdapAuthoritiesPopulator.java
@@ -2,29 +2,21 @@ package gr.serafeim.springldapcustom;
 
 import java.util.Collection;
 import java.util.HashSet;
-
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomLdapAuthoritiesPopulator implements LdapAuthoritiesPopulator {
-
 	@Override
 	public Collection<? extends GrantedAuthority> getGrantedAuthorities(DirContextOperations userData, String username) {
-		
 		Collection<GrantedAuthority> gas = new HashSet<GrantedAuthority>();
-		
 		if(username.equals("spapas")) {
 			gas.add(new SimpleGrantedAuthority("admin"));
 		}
 		gas.add(new SimpleGrantedAuthority("user"));
-		
-		
 		return gas;
 	}
-
 }
